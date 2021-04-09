e213c406b7ba9dea293b6578c38846dea32845e5
Alex Carrega <contact@alexcarrega.com>
2021-04-09 19:13:53 +0200

    Algorithm fix

--- schema/algorithm/instance.py
+++ schema/algorithm/instance.py
@@ -31,9 +31,6 @@ class Algorithm_Instance_Schema(Base_Schema):
     algorithm_catalog_id = Str(required=True, readonly=True, example='ddos-predictor',
                                description='Id of the algorithm in the catalog.',
                                validate=In.apply(Algorithm_Catalog_Document.get_ids), error_messages=In.error_messages)
-    exec_env_id = Str(required=True, readonly=True, example='apache',
-                      description='Id of the execution environment where the algorithm instance is installed.',
-                      validate=In.apply(Exec_Env_Document.get_ids), error_messages=In.error_messages)
     operations = Nested(Algorithm_Instance_Operation_Schema, unknown='INCLUDE',
                         description='List of algorithm instance operations.')
     description = Str(example='Collect system metrics from execution environments.',
