415ace4f4df49392e5994d7cffa35555247da560
Alex Carrega <contact@alexcarrega.com>
2021-12-14 10:43:13 +0100

    Added support for FIWARE's LCP

--- lib/heartbeat.py
+++ lib/heartbeat.py
@@ -3,6 +3,7 @@ from threading import Thread, Timer
 from requests import post
 from requests.exceptions import ConnectionError, ConnectTimeout
 
+from document.agent.catalog import AgentCatalogDocument
 from document.exec_env import ExecEnvDocument
 from lib.http import HTTP_Status
 from lib.token import create_token
@@ -54,6 +55,25 @@ def heartbeat_exec_env(exec_env):
                 log.notice(f'Response: {resp.content}')
             if not lcp.https:
                 lcp.https = False
+            resp = get(f'{schema}://{exec_env.hostname}:{lcp.port}{endpoint_lcp}/poll', timeout=Arg_Reader.db.hb_timeout,
+                       headers={'Authorization': create_token()})
+            if resp.status_code == HTTP_Status.OK:
+                data = resp.json()
+                for agent_cat_data in data.get('agentType', []):
+                    agent_cat = AgentCatalogDocument.get(
+                        id=agent_cat_data.pop('id'))
+                    for field, ac_data in agent_cat_data.items():
+                        setattr(agent_cat, field, ac_data)
+                    agent_cat.save()
+                exec_env_data = data.get('exec_env', {})
+                exec_env.meta.id = exec_env_data.pop('id')
+                for field, lcp_data in exec_env_data.pop('lcp', {}).items():
+                    setattr(exec_env.lcp, field, lcp_data)
+                for field, ee_data in exec_env_data.items():
+                    setattr(exec_env, field, ee_data)
+                log.success(f'Polling established with exec-env {lbl}')
+            else:
+                log.warning(f'Polling not possible with exec-env {lbl}')
             exec_env.save()
         else:
             log.notice(f'Exec-env {lbl} not enabled')
