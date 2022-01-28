8813d87a75bf43f6cbe886590cde18e3ac057658
Alex Carrega <contact@alexcarrega.com>
2022-01-28 09:37:30 +0100

    Major changes

--- dev.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Copyright (c) 2020-2029 Alex Carrega <contact@alexcarrega.com>
-# author: Alex Carrega <contact@alexcarrega.com>
-
-vprof -c cmh main.py --output-file dev/profile.json
